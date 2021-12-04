8fab34f3d780938b8a424a438c1f26b3edacd2b7
vingadorv <vinifranca13@gmail.com>
2021-12-04 16:38:22 -0300

    No functional screen
    A no functional screen for the Series catalog, with the basic crud operations.

---
+++ SeriesOrganizer/SeriesCatalog.Designer.cs
@@ -0,0 +1,339 @@
+﻿
+namespace SeriesOrganizer
+{
+    partial class SeriesCatalog
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SeriesCatalog));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.TextBoxName = new System.Windows.Forms.TextBox();
+            this.TextBoxTotalSeasons = new System.Windows.Forms.TextBox();
+            this.TextBoxGenre = new System.Windows.Forms.TextBox();
+            this.TextBoxCurrentSeason = new System.Windows.Forms.TextBox();
+            this.LabelName = new System.Windows.Forms.Label();
+            this.LabelTotalSeasons = new System.Windows.Forms.Label();
+            this.labelGenre = new System.Windows.Forms.Label();
+            this.LabelCurrentSeason = new System.Windows.Forms.Label();
+            this.LabelCurrentEpisode = new System.Windows.Forms.Label();
+            this.TextBoxCurrentEpisode = new System.Windows.Forms.TextBox();
+            this.ButtonAddSerie = new System.Windows.Forms.Button();
+            this.ButtonUpdate = new System.Windows.Forms.Button();
+            this.ButtonDelete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.TextBoxSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(366, 63);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(550, 516);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // TextBoxName
+            // 
+            this.TextBoxName.BackColor = System.Drawing.Color.LightCyan;
+            this.TextBoxName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxName.Location = new System.Drawing.Point(12, 112);
+            this.TextBoxName.Name = "TextBoxName";
+            this.TextBoxName.Size = new System.Drawing.Size(234, 16);
+            this.TextBoxName.TabIndex = 1;
+            // 
+            // TextBoxTotalSeasons
+            // 
+            this.TextBoxTotalSeasons.BackColor = System.Drawing.Color.LightCyan;
+            this.TextBoxTotalSeasons.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxTotalSeasons.Location = new System.Drawing.Point(12, 186);
+            this.TextBoxTotalSeasons.Name = "TextBoxTotalSeasons";
+            this.TextBoxTotalSeasons.Size = new System.Drawing.Size(234, 16);
+            this.TextBoxTotalSeasons.TabIndex = 2;
+            // 
+            // TextBoxGenre
+            // 
+            this.TextBoxGenre.BackColor = System.Drawing.Color.LightCyan;
+            this.TextBoxGenre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxGenre.Location = new System.Drawing.Point(12, 259);
+            this.TextBoxGenre.Name = "TextBoxGenre";
+            this.TextBoxGenre.Size = new System.Drawing.Size(234, 16);
+            this.TextBoxGenre.TabIndex = 3;
+            // 
+            // TextBoxCurrentSeason
+            // 
+            this.TextBoxCurrentSeason.BackColor = System.Drawing.Color.LightCyan;
+            this.TextBoxCurrentSeason.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxCurrentSeason.Location = new System.Drawing.Point(12, 333);
+            this.TextBoxCurrentSeason.Name = "TextBoxCurrentSeason";
+            this.TextBoxCurrentSeason.Size = new System.Drawing.Size(234, 16);
+            this.TextBoxCurrentSeason.TabIndex = 4;
+            // 
+            // LabelName
+            // 
+            this.LabelName.AutoSize = true;
+            this.LabelName.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelName.ForeColor = System.Drawing.Color.Blue;
+            this.LabelName.Location = new System.Drawing.Point(12, 63);
+            this.LabelName.Name = "LabelName";
+            this.LabelName.Size = new System.Drawing.Size(70, 33);
+            this.LabelName.TabIndex = 5;
+            this.LabelName.Text = "Name";
+            // 
+            // LabelTotalSeasons
+            // 
+            this.LabelTotalSeasons.AutoSize = true;
+            this.LabelTotalSeasons.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelTotalSeasons.ForeColor = System.Drawing.Color.Blue;
+            this.LabelTotalSeasons.Location = new System.Drawing.Point(12, 150);
+            this.LabelTotalSeasons.Name = "LabelTotalSeasons";
+            this.LabelTotalSeasons.Size = new System.Drawing.Size(145, 33);
+            this.LabelTotalSeasons.TabIndex = 6;
+            this.LabelTotalSeasons.Text = "Total Seasons";
+            // 
+            // labelGenre
+            // 
+            this.labelGenre.AutoSize = true;
+            this.labelGenre.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelGenre.ForeColor = System.Drawing.Color.Blue;
+            this.labelGenre.Location = new System.Drawing.Point(12, 223);
+            this.labelGenre.Name = "labelGenre";
+            this.labelGenre.Size = new System.Drawing.Size(73, 33);
+            this.labelGenre.TabIndex = 7;
+            this.labelGenre.Text = "Genre";
+            // 
+            // LabelCurrentSeason
+            // 
+            this.LabelCurrentSeason.AutoSize = true;
+            this.LabelCurrentSeason.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelCurrentSeason.ForeColor = System.Drawing.Color.Blue;
+            this.LabelCurrentSeason.Location = new System.Drawing.Point(12, 297);
+            this.LabelCurrentSeason.Name = "LabelCurrentSeason";
+            this.LabelCurrentSeason.Size = new System.Drawing.Size(165, 33);
+            this.LabelCurrentSeason.TabIndex = 8;
+            this.LabelCurrentSeason.Text = "Current Season";
+            // 
+            // LabelCurrentEpisode
+            // 
+            this.LabelCurrentEpisode.AutoSize = true;
+            this.LabelCurrentEpisode.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LabelCurrentEpisode.ForeColor = System.Drawing.Color.Blue;
+            this.LabelCurrentEpisode.Location = new System.Drawing.Point(12, 376);
+            this.LabelCurrentEpisode.Name = "LabelCurrentEpisode";
+            this.LabelCurrentEpisode.Size = new System.Drawing.Size(172, 33);
+            this.LabelCurrentEpisode.TabIndex = 10;
+            this.LabelCurrentEpisode.Text = "Current Episode";
+            // 
+            // TextBoxCurrentEpisode
+            // 
+            this.TextBoxCurrentEpisode.BackColor = System.Drawing.Color.LightCyan;
+            this.TextBoxCurrentEpisode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxCurrentEpisode.Location = new System.Drawing.Point(12, 412);
+            this.TextBoxCurrentEpisode.Name = "TextBoxCurrentEpisode";
+            this.TextBoxCurrentEpisode.Size = new System.Drawing.Size(234, 16);
+            this.TextBoxCurrentEpisode.TabIndex = 9;
+            // 
+            // ButtonAddSerie
+            // 
+            this.ButtonAddSerie.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ButtonAddSerie.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ButtonAddSerie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonAddSerie.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.ButtonAddSerie.ForeColor = System.Drawing.Color.White;
+            this.ButtonAddSerie.Location = new System.Drawing.Point(12, 464);
+            this.ButtonAddSerie.Name = "ButtonAddSerie";
+            this.ButtonAddSerie.Size = new System.Drawing.Size(108, 38);
+            this.ButtonAddSerie.TabIndex = 11;
+            this.ButtonAddSerie.Text = "ADD";
+            this.ButtonAddSerie.UseVisualStyleBackColor = false;
+            // 
+            // ButtonUpdate
+            // 
+            this.ButtonUpdate.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ButtonUpdate.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ButtonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonUpdate.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.ButtonUpdate.ForeColor = System.Drawing.Color.White;
+            this.ButtonUpdate.Location = new System.Drawing.Point(165, 464);
+            this.ButtonUpdate.Name = "ButtonUpdate";
+            this.ButtonUpdate.Size = new System.Drawing.Size(118, 38);
+            this.ButtonUpdate.TabIndex = 12;
+            this.ButtonUpdate.Text = "UPDATE";
+            this.ButtonUpdate.UseVisualStyleBackColor = false;
+            // 
+            // ButtonDelete
+            // 
+            this.ButtonDelete.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ButtonDelete.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ButtonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonDelete.Font = new System.Drawing.Font("Arial", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.ButtonDelete.ForeColor = System.Drawing.Color.White;
+            this.ButtonDelete.Location = new System.Drawing.Point(80, 541);
+            this.ButtonDelete.Name = "ButtonDelete";
+            this.ButtonDelete.Size = new System.Drawing.Size(120, 38);
+            this.ButtonDelete.TabIndex = 13;
+            this.ButtonDelete.Text = "DELETE";
+            this.ButtonDelete.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(619, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.Location = new System.Drawing.Point(12, 362);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 1);
+            this.panel1.TabIndex = 15;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Navy;
+            this.panel2.Location = new System.Drawing.Point(12, 134);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(236, 1);
+            this.panel2.TabIndex = 16;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Navy;
+            this.panel4.Location = new System.Drawing.Point(12, 215);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(236, 1);
+            this.panel4.TabIndex = 18;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Navy;
+            this.panel3.Location = new System.Drawing.Point(12, 441);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(236, 1);
+            this.panel3.TabIndex = 19;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Navy;
+            this.panel5.Location = new System.Drawing.Point(12, 288);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(236, 1);
+            this.panel5.TabIndex = 20;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Navy;
+            this.panel6.Location = new System.Drawing.Point(367, 56);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(236, 1);
+            this.panel6.TabIndex = 22;
+            // 
+            // TextBoxSearch
+            // 
+            this.TextBoxSearch.BackColor = System.Drawing.Color.LightCyan;
+            this.TextBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBoxSearch.Location = new System.Drawing.Point(366, 34);
+            this.TextBoxSearch.Name = "TextBoxSearch";
+            this.TextBoxSearch.Size = new System.Drawing.Size(234, 16);
+            this.TextBoxSearch.TabIndex = 21;
+            // 
+            // SeriesCatalog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(943, 619);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.TextBoxSearch);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ButtonDelete);
+            this.Controls.Add(this.ButtonUpdate);
+            this.Controls.Add(this.ButtonAddSerie);
+            this.Controls.Add(this.LabelCurrentEpisode);
+            this.Controls.Add(this.TextBoxCurrentEpisode);
+            this.Controls.Add(this.LabelCurrentSeason);
+            this.Controls.Add(this.labelGenre);
+            this.Controls.Add(this.LabelTotalSeasons);
+            this.Controls.Add(this.LabelName);
+            this.Controls.Add(this.TextBoxCurrentSeason);
+            this.Controls.Add(this.TextBoxGenre);
+            this.Controls.Add(this.TextBoxTotalSeasons);
+            this.Controls.Add(this.TextBoxName);
+            this.Controls.Add(this.listView1);
+            this.Name = "SeriesCatalog";
+            this.Text = "Catalog";
+            this.Load += new System.EventHandler(this.SeriesCatalog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox TextBoxName;
+        private System.Windows.Forms.TextBox TextBoxTotalSeasons;
+        private System.Windows.Forms.TextBox TextBoxGenre;
+        private System.Windows.Forms.TextBox TextBoxCurrentSeason;
+        private System.Windows.Forms.Label LabelName;
+        private System.Windows.Forms.Label LabelTotalSeasons;
+        private System.Windows.Forms.Label labelGenre;
+        private System.Windows.Forms.Label LabelCurrentSeason;
+        private System.Windows.Forms.Label LabelCurrentEpisode;
+        private System.Windows.Forms.TextBox TextBoxCurrentEpisode;
+        private System.Windows.Forms.Button ButtonAddSerie;
+        private System.Windows.Forms.Button ButtonUpdate;
+        private System.Windows.Forms.Button ButtonDelete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox TextBoxSearch;
+    }
+}
+
